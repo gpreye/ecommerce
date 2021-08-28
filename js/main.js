4431189dd5dbafcec798d8a5ecaed1b369e69318
gpreye <46998526+gpreye@users.noreply.github.com>
2021-08-28 09:11:42 -1100

    Initial project

---
+++ js/main.js
@@ -0,0 +1,6 @@
+// Nav Slide
+let hamBurger = document.querySelector("#hamburger");
+hamBurger.addEventListener("click", function () {
+    document.querySelector(".nav-links").classList.toggle("show");
+       document.querySelector(".burger").classList.toggle("show");
+})
